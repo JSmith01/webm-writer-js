099e412e82b3620de288525dcbd28fde8ccb9ca0
Dmitry Zlygin <dzlygin@gmail.com>
2022-12-12 15:06:10 +0400

    get rid of jQuery

--- test/electron/run.js
+++ test/electron/run.js
@@ -1,67 +1,65 @@
-(window => {
-	const
-		WebMWriterProxy = window["WebMWriterProxy"];
+const WebMWriterProxy = window["WebMWriterProxy"];
 
-	function renderToFile() {
-		return WebMWriterProxy.init({
-			frameRate: 30
-		}).then(writer => new Promise((resolve, reject) => {
-			let
-				canvas = document.getElementById("canvas"),
-				context = canvas.getContext("2d"),
+function renderToFile() {
+	return WebMWriterProxy.init({
+		frameRate: 30
+	}).then(writer => new Promise((resolve, reject) => {
+		let
+			canvas = document.getElementById("canvas"),
+			context = canvas.getContext("2d"),
 
-				helloWorld = "Hello World",
+			helloWorld = "Hello World",
 
-				textWidth,
-				textHeight = 20,
+			textWidth,
+			textHeight = 20,
 
-				posX = 0, posY = 0,
-				directionX = 1, directionY = 1,
+			posX = 0, posY = 0,
+			directionX = 1, directionY = 1,
 
-				stepSize = 15,
-				maxFrames = 200;
+			stepSize = 15,
+			maxFrames = 200;
 
-			context.font = textHeight + "pt Arial";
-			textWidth = context.measureText(helloWorld).width;
+		context.font = textHeight + "pt Arial";
+		textWidth = context.measureText(helloWorld).width;
 
-			let renderFrame = function (frameIndex) {
-				context.fillStyle = "#eee";
-				context.fillRect(0, 0, canvas.width, canvas.height);
+		let renderFrame = function (frameIndex) {
+			context.fillStyle = "#eee";
+			context.fillRect(0, 0, canvas.width, canvas.height);
 
-				context.fillStyle = "black";
-				context.fillText(helloWorld, posX, posY + textHeight);
+			context.fillStyle = "black";
+			context.fillText(helloWorld, posX, posY + textHeight);
 
-				writer.addFrame(canvas);
+			writer.addFrame(canvas);
 
-				posX += directionX * stepSize;
-				posY += directionY * stepSize;
+			posX += directionX * stepSize;
+			posY += directionY * stepSize;
 
-				if (posX < 0 || posX + textWidth >= canvas.width) {
-					directionX *= -1;
+			if (posX < 0 || posX + textWidth >= canvas.width) {
+				directionX *= -1;
+			}
+			if (posY < 0 || posY + textHeight >= canvas.height) {
+				directionY *= -1;
+			}
+
+			setTimeout(function () {
+				if (frameIndex === maxFrames) {
+					writer.complete()
+						.then(() => writer.readAsDataURL())
+						.then(resolve)
+						.catch(reject);
+				} else {
+					renderFrame(frameIndex + 1);
 				}
-				if (posY < 0 || posY + textHeight >= canvas.height) {
-					directionY *= -1;
-				}
+			}, 0);
+		};
 
-				setTimeout(function () {
-					if (frameIndex === maxFrames) {
-						writer.complete()
-							.then(() => writer.readAsDataURL())
-							.then(resolve)
-							.catch(reject);
-					} else {
-						renderFrame(frameIndex + 1);
-					}
-				}, 0);
-			};
+		renderFrame(0);
+	}));
+}
 
-			renderFrame(0);
-		}));
-	}
-
-	$(document).ready(() => {
-		renderToFile().then(dataURL => {
-			$("video").attr('src', dataURL);
-		});
-	});
-})(window);
+document.addEventListener(
+	'DOMContentLoaded',
+	() => renderToFile().then(dataUrl => {
+		document.getElementsByTagName('video')[0].src = dataUrl;
+	})
+);
